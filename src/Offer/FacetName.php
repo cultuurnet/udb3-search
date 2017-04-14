29c12ec33693c077db776d543e8fe1a44e1ebdb5
Bert Ramakers <bertramakers@me.com>
2017-04-14 16:02:33 +0200

    III-1955: Add facets meant to be added later to make testing easier

--- src/Offer/FacetName.php
+++ src/Offer/FacetName.php
@@ -6,8 +6,14 @@ use ValueObjects\Enum\Enum;
 
 /**
  * @method static FacetName REGIONS()
+ * @method static FacetName TYPES()
+ * @method static FacetName THEMES()
+ * @method static FacetName FACILITIES()
  */
 class FacetName extends Enum
 {
     const REGIONS = 'regions';
+    const TYPES = 'types';
+    const THEMES = 'themes';
+    const FACILITIES = 'facilities';
 }
